948e87473a6e8b530f0b4ac4cc5602c064b3ffd5
liuguangzhen01 <15317077256@163.com>
2019-07-07 11:15:51 +0800

    修改布局页面

--- src/layouts/GlobalLayout.vue
+++ src/layouts/GlobalLayout.vue
@@ -2,15 +2,35 @@
   a-layout(id="components-layout-custom-trigger")
     a-layout-sider(:trigger="null" collapsible v-model="collapsed")
       div(class="logo")
-      a-menu(theme="dark" mode="inline" :defaultSelectedKeys="['1']")
-        a-menu-item(key="1")
-          router-link(to="AntDesignVue" :style="{ color: 'aqua', float: 'left' }")
+        img(v-if="!collapsed" src="../assets/logo-unfold.png")
+        img(v-else src="../assets/logo-fold.png")
+      a-menu(theme="dark" mode="inline" :defaultOpenKeys="['submenu1']" :defaultSelectedKeys="['11']" :style="{ height: '100%', borderRight: 0 }")
+        a-sub-menu(key="submenu1")
+          span(slot="title" class="menu-title")
             a-icon(type="profile")
             span ant-design-vue
-        a-menu-item(key="2")
-          router-link(to="HelloWorld" :style="{ color: 'aqua', float: 'left' }")
+          a-menu-item(key="11")
+            router-link(to="AntDesignVue" class="link")
+              span ant-design-vue
+          a-menu-item(key="12")
+            router-link(to="" class="link")
+              span option2
+          a-menu-item(key="13")
+            router-link(to="" class="link")
+              span option3
+        a-sub-menu(key="submenu2")
+          span(slot="title" class="menu-title")
             a-icon(type="profile")
-            span hello-world
+            span vue.js
+          a-menu-item(key="21")
+            router-link(to="HelloWorld" class="link")
+              span hello-world
+          a-menu-item(key="22")
+            router-link(to="" class="link")
+              span option2
+          a-menu-item(key="23")
+            router-link(to="" class="link")
+              span option3
     a-layout
       a-layout-header(style="background: #fff; padding: 0")
         a-icon(class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="()=> collapsed = !collapsed")
@@ -44,14 +64,19 @@ export default {
   color: #1890ff;
 }
 #components-layout-custom-trigger .logo {
-  height: 32px;
+  height: 64px;
   background: rgba(255,255,255,.2);
-  margin: 16px;
 }
 #components-layout-custom-trigger .content {
   margin: 24px 16px;
   padding: 24px;
   background: #fff;
   min-height: 280px;
+}
+#components-layout-custom-trigger .menu-title {
+  float: left;
+}
+#components-layout-custom-trigger .link {
+  float: left;
 }
 </style>
