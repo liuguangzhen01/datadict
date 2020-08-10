9deab086224446cbf1d57b22d4789c72a712772c
liuguangzhen <15317077256@163.com>
2020-08-10 10:35:02 +0800

    拖拽排序学习

--- src/views/vueJs/VueDraggable.vue
+++ src/views/vueJs/VueDraggable.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
-  draggable(:list="list" ghost-class="ghost")
-    div(v-for="item in list" :key="item.id" class="item-style") {{item.name}}
+  div
+    a-button(@click="openModal") 打开拖拽弹框
+    a-modal(title="拖拽排序" v-model="visible" @ok="handleOk")
+      draggable(:list="list" :options="options" tag="ul" class="list-group")
+        transition-group(type="transition" name="flip-list")
+          li(class="list-group-item" v-for="(item, index) in list" :key="item.id")
+            a-icon(type="tag")
+            span {{item.name}}
+            span {{index + 1}}
 </template>
 
 <script>
@@ -8,6 +15,18 @@ export default {
   name: 'VueDraggable',
   data () {
     return {
+      visible: false,
+      dialogStyle: {
+        height: '200px',
+        overflowY: 'auto'
+      },
+      options: {
+        animation: 200, // 动画时间
+        disabled: false, // false可拖拽，true不可拖拽
+        ghostClass: 'ghost',
+        // forceFallback: true,
+        scroll: true
+      },
       list: [
         {
           id: '1',
@@ -28,11 +47,46 @@ export default {
         {
           id: '5',
           name: 'Sam'
+        },
+        {
+          id: '6',
+          name: 'Tim'
+        },
+        {
+          id: '7',
+          name: 'Tom'
+        },
+        {
+          id: '8',
+          name: 'Leo'
+        },
+        {
+          id: '9',
+          name: 'Tony'
+        },
+        {
+          id: '10',
+          name: 'Mark'
+        },
+        {
+          id: '11',
+          name: 'Jake'
+        },
+        {
+          id: '12',
+          name: 'Max'
         }
       ]
     }
   },
-  methods: {},
+  methods: {
+    openModal () {
+      this.visible = true
+    },
+    handleOk () {
+      this.visible = false
+    }
+  },
   watch: {
     list () {
       console.log(this.list)
@@ -42,10 +96,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.item-style {
-  cursor: pointer;
-  border: 1px solid red;
-  width: 100px;
-  margin-bottom: 12px;
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.list-group {
+  height: 300px;
+  overflow-y: auto;
+  .list-group-item {
+    cursor: move;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 16px;
+    border: 1px solid #fff;
+    height: 40px;
+    width: 360px;
+    text-align: center;
+    background: #ccc;
+  }
 }
 </style>
